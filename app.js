4f7f478d7377a50506c99fc65adf1ce5c0088857
Philip Kroupoderov <philip.kroupoderov@gmail.com>
2019-01-16 19:51:36 -0500

    Removed a portion from the node.js file responsible for handling static assets after realizing that these assets are cached in the browser

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('./public'))
 
-app.get("/static/:folder?/:file", (req, res, next) => {
-
-    const { folder } = req.params
-    const { file } = req.params
-
-    let fileName = path.join(__dirname + '/static/' + folder + '/' + file)
-
-    if (fs.existsSync(fileName)) {
-        res.sendFile(fileName)
-    } else {
-        next()
-    }
-})
-
 app.use((req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
