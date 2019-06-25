2e3296994a86cb6f7d458f2ccf6eceeed25fe541
Philip Kroupoderov <philipkroupoderov@Philips-MacBook-Pro.local>
2019-06-24 21:32:13 -0400

    Created A CDN on AWS to distribute static assets

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express()
 
 const port = process.env.PORT || 8080
 
-app.use(express.static('./public'))
-
 app.use((req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
