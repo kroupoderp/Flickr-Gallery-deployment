b06e96db37a28964ac861fe4b89d1518e465e21d
Philip Kroupoderov <philipkroupoderov@Philips-MacBook-Pro.local>
2019-09-15 20:04:49 -0400

    commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 
 const port = process.env.PORT || 8080
 
+app.use(express.static('./public'))
+
 app.use((req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
